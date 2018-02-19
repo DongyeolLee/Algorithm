c6f3f143c26dadeb7a1208ec7ccffb94211c2bb2
DongyeolLee <dongyeol8@gmail.com>
2018-02-19 11:43:40 +0900

    2*n 타일링

---
+++ dynamic programming/2*n 타일링/11726.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i = 2; i <= n; i ++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
